ebb904388376cbcaa7d0780112f2de7cefe9819d
martom87 <marcintomala87@gmail.com>
2021-05-28 15:52:24 +0200

    adds sidekiq draft configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq' # mount Sidekiq::Web in your Rails app
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
