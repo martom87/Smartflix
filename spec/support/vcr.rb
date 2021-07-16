d430ac695e9578b2fd1690b3227a92bfc880fdc5
martom87 <marcintomala87@gmail.com>
2021-07-16 13:50:13 +0200

    Add enviroments sections into credentials and update their references

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,6 +3,6 @@
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
-  config.ignore_localhost = true
-  config.filter_sensitive_data('apikey') { Rails.application.credentials.omdb[:access_key_id] }
+  config.ignore_localhost = true  
+  config.filter_sensitive_data('apikey') { Rails.application.credentials[Rails.env.to_sym][:omdb][:access_key_id] }
 end
