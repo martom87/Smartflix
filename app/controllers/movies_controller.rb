f4c0f01b91c3c14de8c2b84b6772e5e2e6fe43c8
Marcin Tomala <marcintomala87@gmail.com>
2021-07-30 12:56:00 +0200

    Add controller action for entry point (#5)
    * Fix rubocop offeces
    
    * Add draft controller action
    
    * Add response messages
    
    * Use worker to create movie
    
    * Fix worker execution

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class MoviesController < ApplicationController
+
+  def show
+    movie = Movie.find_by(title: params[:title])
+
+    if movie
+
+      render json: movie, status: :ok
+    else
+      CreateMovieWorker.perform_async(params[:title])
+      render json: { message: "The film #{params[:title]} will be downloaded" }, status: :not_found
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 require 'sidekiq/web'
 Rails.application.routes.draw do
-  mount Sidekiq::Web => '/sidekiq' # mount Sidekiq::Web in your Rails app
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web => '/sidekiq'
+
+  resources :movies, only: %i[show], param: :title
 end
